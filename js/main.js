4563e01b893ca1bdc2d618b23958f4800bc78e24
eswaran1902 <eswaran.jj@gmail.com>
2025-09-10 00:51:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U DA PAPA').split('')
+    const titles = ('I LOVE U DA PAPA💕❤️').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
